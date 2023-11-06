7639dfccea06864d5d10a7384cf6285796572ed0
Georgi Kotakov <g.kotakov@studioweb.bg>
2023-11-06 15:12:52 +0200

    kotakov

--- core/sitemaps/index.php
+++ core/sitemaps/index.php
@@ -2,7 +2,7 @@
 define( "_VALID_PHP", true);
 require_once '../autoload.php'; ?>
 
-<?xml version="1.0" encoding="UTF-8"?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?' ?>
 <sitemapindex  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 	<sitemap>
 		<loc><?= SITEURL.'/'.$lang->language ?>/sitemaps/sitemap_products</loc>
