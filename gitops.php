5b40a185964308112dad82b9c1544166b136a549
Georgi Kotakov <g.kotakov@studioweb.bg>
2024-04-17 16:27:18 +0300

    new

--- gitops.php
+++ gitops.php
@@ -54,6 +54,7 @@ if(post('saas_key')){
 	// save new content
 	file_put_contents($file, $newContent);
 	
+	$user->logout();
 	header('Location: /gitops.php');
 	die();
 }
@@ -111,6 +112,10 @@ function deleteFiles($target) {
 	}
 }
 
+if(post('git_pull')){
+	shell_exec("git pull");
+}
+
 ?>
 
 
@@ -120,21 +125,24 @@ function deleteFiles($target) {
 		<title>GitOps</title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<style>.red{color: red;}</style>
+		<style>.red{color: red;}.table td{padding: 10px}</style>
 	</head>
 	<body>
 		
 		
 		<div>
 			
-				<table>
-					<tr>
-						<td>Repo: </td>
-						<td><?php  echo $curent_repo ?></td>
-					</tr>
+				<table class='table'>
+					
 					<tr>
 						<td>Project Name: </td>
 						<td><?php  echo $core->site_name ?></td>
+					</tr>
+					<tr>
+						<td>Current Repo: </td>
+						<td><form method="post"><?php  echo $curent_repo ?> <input type="hidden" name="git_pull" value='1'><button>Git Pull</button>
+							<i>*get latest update</i></form>
+						</td>
 					</tr>
 					<tr>
 						<td>Saas Key: </td>
@@ -154,7 +162,7 @@ function deleteFiles($target) {
 				<div><?php if(!$github_token) echo "Please, enter github token: <a href='https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens' target='_blank'>Help</a>"; ?></div>
 			 </div>
 			 <div style="<?php if($hide_table) echo "display:none;"; ?>">
-					Push to repo: <br/><i>*you can push only in repo of project</i>
+					Push to repo: <br/><i>*you can push only in project repo</i>
 				   <form method="post">
 					   <input type="hidden" name="upload_repo"  value="1" />
 					   <input type="text" placeholder="GitHub username" value="<?php echo $core->site_name; ?>" disabled/>	
@@ -162,7 +170,7 @@ function deleteFiles($target) {
 				   </form>
 				   <br/>
 				   <br/>
-				   Fetch from GitHub:
+				   Fetch other repo from GitHub:
 				   <form method="post">
 					   <input type="hidden" name='download' value='1' />
 					   <select name='repo'>
